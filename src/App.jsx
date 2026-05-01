44cce1e405d9af2e5c288a80866a4d6b641f5c9f
Beharudin Mohammed <89333678+Beharudin@users.noreply.github.com>
2026-05-01 19:48:49 +0300

    Added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -17,7 +19,7 @@ const App = () => {
       <About />
       <Art />
       <Menu />
-      {/* <Contact /> */}
+      <Contact />
     </main>
   );
 };
