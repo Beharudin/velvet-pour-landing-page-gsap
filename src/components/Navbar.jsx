14f9ec7fe2a0a68d8577d576d8d6b9f199c83ace
Beharudin Mohammed <89333678+Beharudin@users.noreply.github.com>
2026-05-01 15:38:40 +0300

    Added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,21 +4,25 @@ import { useGSAP } from "@gsap/react";
 import { navLinks } from "../../constants/index.js";
 
 const Navbar = () => {
-  //  useGSAP(() => {
-  // 	const navTween = gsap.timeline({
-  // 	 scrollTrigger: {
-  // 		trigger: 'nav',
-  // 		start: 'bottom top'
-  // 	 }
-  // 	});
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
 
-  // 	navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
-  // 	 backgroundColor: '#00000050',
-  // 	 backgroundFilter: 'blur(10px)',
-  // 	 duration: 1,
-  // 	 ease: 'power1.inOut'
-  // 	});
-  //  })
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      },
+    );
+  });
 
   return (
     <nav>
